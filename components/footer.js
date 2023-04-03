b78a2b4d56fbfc14ada3abcd8661015965632a4c
ingui-n <inguin@email.cz>
2023-04-03 16:09:48 +0200

    updated footer.js

--- components/footer.js
+++ components/footer.js
@@ -1,37 +1,31 @@
-
-import * as m from '@mui/material';
+import {Typography} from '@mui/material';
 
 export default function Footer() {
-    return (
-       <div> 
-        <footer>
-        <m.Typography variant="body1"><strong>Powered by</strong> Vašek a Vito</m.Typography>
-        </footer>
+  return (
+    <>
+      <footer>
+        <Typography variant="body1"><strong>Powered by</strong> Vašek a Vito</Typography>
+      </footer>
 
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
 
-
-
-<style jsx>{`
-main {
-  padding: 5rem 0;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+        footer {
+          width: 100%;
+          height: 4rem;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+      `}</style>
+    </>
+  );
 }
-footer {
-  width: 100%;
-  height: 4rem;
-  border-top: 1px solid #eaeaea;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
-
-}
-`}</style>
-    </div>
-    )
-}
-
