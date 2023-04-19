0b28173a04a14a5989eb5d84e67792df245d204c
stredvi1 <stredvi1@uhk.cz>
2023-04-19 20:55:53 +0200

    edit form

--- components/projectForm.js
+++ components/projectForm.js
@@ -1,7 +1,8 @@
 "use client"
 import {useFormik} from "formik";
-import {Button, Stack, TextField} from "@mui/material";
+import {Button, Stack, TextField, Paper, Box} from "@mui/material";
 import {ProjectScheme} from "./Schemes/projectScheme";
+import React from "react";
 
 export default function ProjectForm() {
     const formik = useFormik({
@@ -20,53 +21,63 @@ export default function ProjectForm() {
     return (
         <>
             <form onSubmit={formik.handleSubmit}>
-                <Stack direction="column" spacing={2} sx={{
-                    width: 500,
-                    maxWidth: '100%',
-                }}>
-                    <TextField
-                        id="projectName"
-                        label="Název projektu"
-                        required
-                        variant="outlined"
-                        {...formik.getFieldProps('projectName')}
-                        error={formik.touched.projectName && Boolean(formik.errors.projectName)}
-                        helperText={formik.touched.projectName && formik.errors.projectName}/>
-                    <TextField
-                        id="budget"
-                        label="Rozpočet"
-                        placeholder="0.00"
-                        required
-                        variant="outlined"
-                        {...formik.getFieldProps('budget')}
-                        error={formik.touched.budget && Boolean(formik.errors.budget)}
-                        helperText={formik.touched.budget && formik.errors.budget}/>
-                    <TextField
-                        id="deadline"
-                        label="Deadline"
-                        placeholder="rrrr-mm-dd"
-                        required
-                        variant="outlined"
-                        {...formik.getFieldProps('deadline')}
-                        error={formik.touched.deadline && Boolean(formik.errors.deadline)}
-                        helperText={formik.touched.deadline && formik.errors.deadline}/>
-                    <TextField
-                        id="description"
-                        label="Popis"
-                        required
-                        variant="outlined"
-                        multiline
-                        rows={4}
-                        {...formik.getFieldProps('description')}
-                        error={formik.touched.description && Boolean(formik.errors.description)}
-                        helperText={formik.touched.description && formik.errors.description}/>
-                    <Button onClick={() => {
-                        alert('clicked');
-                    }}
-                            variant="outlined"
-                            color="success"
-                    >Vyvořit projekt
-                    </Button>
+                <Stack direction="column" spacing={6}
+                       justifyContent="center"
+                       alignItems="center">
+
+                    <Paper elevation={3}>
+                        <Box padding={4}>
+                            <TextField
+                                id="projectName"
+                                label="Název projektu"
+                                required
+                                variant="outlined"
+                                {...formik.getFieldProps('projectName')}
+                                error={formik.touched.projectName && Boolean(formik.errors.projectName)}
+                                helperText={formik.touched.projectName && formik.errors.projectName}/>
+
+                            <TextField
+                                id="budget"
+                                label="Rozpočet"
+                                placeholder="0.00"
+                                required
+                                variant="outlined"
+                                {...formik.getFieldProps('budget')}
+                                error={formik.touched.budget && Boolean(formik.errors.budget)}
+                                helperText={formik.touched.budget && formik.errors.budget}/>
+
+                            <TextField
+                                id="deadline"
+                                label="Deadline"
+                                placeholder="rrrr-mm-dd"
+                                required
+                                variant="outlined"
+                                {...formik.getFieldProps('deadline')}
+                                error={formik.touched.deadline && Boolean(formik.errors.deadline)}
+                                helperText={formik.touched.deadline && formik.errors.deadline}/>
+
+                            <TextField
+                                id="description"
+                                label="Popis"
+                                required
+                                variant="outlined"
+                                multiline
+                                rows={6}
+                                {...formik.getFieldProps('description')}
+                                error={formik.touched.description && Boolean(formik.errors.description)}
+                                helperText={formik.touched.description && formik.errors.description}/>
+
+                            <Button
+                                variant="text"
+                                href="./overview">Zrušit
+                            </Button>
+                            <Button
+                                type="submit"
+                                variant="contained"
+                            >Vyvořit projekt
+                            </Button>
+                        </Box>
+                    </Paper>
                 </Stack>
             </form>
         </>
--- components/registrationForm.js
+++ components/registrationForm.js
@@ -63,8 +63,6 @@ export default function RegistrationForm() {
     }
 
 
-
-
     const userTypes = UserTypes();
 
     return (
