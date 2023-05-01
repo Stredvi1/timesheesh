a1a13137e73033bdbe0aa75026aa27d3d9149fa5
stredvi1 <stredvi1@uhk.cz>
2023-05-01 20:30:59 +0200

    edited NAV

--- components/navbar.js
+++ components/navbar.js
@@ -6,40 +6,49 @@ import Button from '@mui/material/Button';
 import {LoupeOutlined, HomeOutlined} from '@mui/icons-material/';
 import Link from "next/link";
 import {useSession} from "next-auth/react";
-import {IconButton} from "@mui/material";
+import {IconButton, Stack} from "@mui/material";
 import {useRouter} from "next/router";
 
 
 export default function ButtonAppBar() {
-    const {data:session} = useSession();
+    const {data: session} = useSession();
     const router = useRouter();
 
     function home() {
-        // if(router.pathname !=== "/overview") {
-        //      // router.push("/overview")
-        // }
+
+        if (router.pathname !== "/overview") {
+            router.push("/overview");
+        }
     }
 
     return (
         <Box sx={{flexGrow: 1}}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        onClick={home()}>
-                        <HomeOutlined sx={{align: "center"}}/>
-                    </IconButton>
-
-                    <LoupeOutlined/>
-                    <Typography variant="h6" component="div" sx={{flexGrow: 1}}>
-                        TimeShift
-                    </Typography>
-                    <Link href='/overview' >
-                    </Link>
+                    <Stack
+                        direction="row"
+                        spacing={4}
+                        component="div" sx={{flexGrow: 1}}
+                        alignItems="center">
+                        <Box
+                            sx={{ display: 'flex' }}>
+                            <LoupeOutlined/>
+                            <Typography variant="h6">
+                                TimeShift
+                            </Typography>
+                        </Box>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{mr: 2}}
+                            onClick={() => {
+                                home();
+                            }}>
+                            <HomeOutlined/>
+                        </IconButton>
+                    </Stack>
                     <Link href='/newUser'>
                         <Button color="inherit">Přidat uživatele</Button>
                     </Link>
